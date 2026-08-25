90241eb61324323c8faeb3a4f5f1a77ed0620987
Chiesaac7 <isoportosa@est.utn.ac.cr>
2026-08-25 01:54:18 -0600

    Gestor de Servicios
    importé servicio adicional y el arraylist, tambien agregue generacion de cdigo de servicio y metodos para agregar, buscar, actualizar, eliminar y obtener los servicios ya registrados

---
+++ src/Modelo/gestores/GestorServicios.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.gestores;
+import Modelo.ServicioAdicional;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author isaac
+ */
+public class GestorServicios {
+ 
+    private ArrayList<ServicioAdicional> servicios;
+    private int siguienteCodigo;
+    public GestorServicios(){
+        servicios= new ArrayList<>();
+        siguienteCodigo= 1;
+    }
+    
+    public ServicioAdicional agregar(String nombre, String descipcion, double precio){
+        
+        String codigo= "S"+siguienteCodigo;
+        
+        ServicioAdicional servicio= new ServicioAdicional(codigo, nombre, descipcion, precio);
+        
+        servicios.add(servicio);
+        siguienteCodigo++;
+        
+        return servicio;
+    }
+    
+    public ServicioAdicional buscar(String codigo){
+        
+        for(ServicioAdicional servicio: servicios){
+            if(servicio.getCodigo().equals(codigo)){
+                return servicio;
+            }
+        }
+        return null;
+    }
+    
+    public boolean actualizar(String codigo, String descripcion, double precio){
+        
+        ServicioAdicional servicio=buscar(codigo);
+        
+        if(servicio!=null){
+            servicio.setDescripcion(descripcion);
+            servicio.setPrecio(precio);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean eliminar(String codigo){
+        
+        ServicioAdicional servicio=buscar(codigo);
+        
+        if(servicio!=null){
+            servicios.remove(servicio);
+            return true;
+        }
+        return false;
+    }
+    public ArrayList<ServicioAdicional> getServicios(){
+        return servicios;
+    }
+}
