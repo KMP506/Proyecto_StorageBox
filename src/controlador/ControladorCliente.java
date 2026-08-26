e784c3c65dd42b08e0458581d24f66e94307159c
Jairo <montoysjairito@gmail.com>
2026-08-25 23:51:44 -0600

    Se crean nuevas clases para el controlador
    Se crearon los controladores cliente y espacio

--- src/controlador/ControladorCliente.java
+++ src/controlador/ControladorCliente.java
@@ -4,10 +4,53 @@
  */
 package controlador;
 
+
+import Modelo.Cliente;
+import Modelo.SistemaStorageBox;
+import Modelo.excepciones.ClienteConContratosException;
+import Modelo.excepciones.IdentificacionDuplicadaException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 /**
  *
  * @author monto
  */
 public class ControladorCliente {
-    
+ 
+    private SistemaStorageBox sistema;
+ 
+    public ControladorCliente(SistemaStorageBox sistema) {
+        this.sistema = sistema;
+    }
+ 
+    public void agregarCliente(String id, String nombreCompleto, String telefono,
+            LocalDate fechaNacimiento, String correoElectronico)
+            throws IdentificacionDuplicadaException {
+ 
+        Cliente cliente = new Cliente(id, nombreCompleto, telefono,
+                fechaNacimiento, correoElectronico);
+ 
+        sistema.agregarCliente(cliente);
+    }
+ 
+    public Cliente buscarCliente(String id) {
+        return sistema.buscarCliente(id);
+    }
+ 
+    public boolean actualizarCliente(String id, String nombre,
+            String telefono, String correo) {
+ 
+        return sistema.actualizarCliente(id, nombre, telefono, correo);
+    }
+ 
+    public boolean eliminarCliente(String id)
+            throws ClienteConContratosException {
+ 
+        return sistema.eliminarCliente(id);
+    }
+ 
+    public ArrayList<Cliente> listarClientes() {
+        return sistema.getGestorClientes().getClientes();
+    }
+ 
 }
---
+++ src/controlador/ControladorEspacio.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+
+import Modelo.Espacio;
+import Modelo.SistemaStorageBox;
+import Modelo.TipoEspacio;
+import Modelo.excepciones.EspacioDuplicadoException;
+import Modelo.excepciones.EspacioOcupadoException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+/**
+ *
+ * @author monto
+ */
+public class ControladorEspacio {
+ 
+    private SistemaStorageBox sistema;
+ 
+    public ControladorEspacio(SistemaStorageBox sistema) {
+        this.sistema = sistema;
+    }
+ 
+    public void agregarEspacio(String numeroEspacio, TipoEspacio tipo)
+            throws EspacioDuplicadoException {
+ 
+        Espacio espacio = new Espacio(numeroEspacio, tipo);
+        sistema.agregarEspacio(espacio);
+    }
+ 
+    public Espacio buscarEspacio(String numeroEspacio) {
+        return sistema.buscarEspacio(numeroEspacio);
+    }
+ 
+    public boolean actualizarEspacio(String numeroEspacio,
+            TipoEspacio tipo, double tamano, double precio) {
+ 
+        return sistema.actualizarEspacio(numeroEspacio, tipo, tamano, precio);
+    }
+ 
+    public boolean eliminarEspacio(String numeroEspacio)
+            throws EspacioOcupadoException {
+ 
+        return sistema.eliminarEspacio(numeroEspacio);
+    }
+ 
+    public ArrayList<Espacio> listarEspacios() {
+        HashMap<String, Espacio> espacios = sistema.getGestorEspacios().getEspacios();
+        return new ArrayList<>(espacios.values());
+    }
+ 
+    public int cantidadEspaciosDisponibles(TipoEspacio tipo,
+            LocalDate fechaInicio, LocalDate fechaFin) {
+ 
+        return sistema.cantidadEspaciosDisponibles(tipo, fechaInicio, fechaFin);
+    }
+ 
+}
+ 
