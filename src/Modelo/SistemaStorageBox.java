b7bf0494aa2e276978513aa87d8414e789a40197
Efrain Fernandez <effernandezcr@est.utn.ac.cr>
2026-08-25 20:08:12 -0600

    integrar gestores en SistemaStorageBox
    Agregué los gestores de espacios, servicios, empleados y contratos a SistemaStorageBox, junto con su inicialización en el constructor y sus métodos de acceso.

--- src/Modelo/SistemaStorageBox.java
+++ src/Modelo/SistemaStorageBox.java
@@ -5,17 +5,42 @@
 package Modelo;
 
 import Modelo.gestores.GestorClientes;
+import Modelo.gestores.GestorContratos;
+import Modelo.gestores.GestorEmpleados;
+import Modelo.gestores.GestorEspacios;
+import Modelo.gestores.GestorServicios;
 
 /**
  *
  * @author kevin
  */
 public class SistemaStorageBox {
-     private GestorClientes gestorClientes;
+    private GestorClientes gestorClientes;
+    private GestorEspacios gestorEspacios;
+    private GestorServicios gestorServicios;
+    private GestorEmpleados gestorEmpleados;
+    private GestorContratos gestorContratos;
+
     public SistemaStorageBox() {
         gestorClientes = new GestorClientes();
+        gestorEspacios = new GestorEspacios();
+        gestorServicios = new GestorServicios();
+        gestorEmpleados = new GestorEmpleados();
+        gestorContratos = new GestorContratos();
     }
     public GestorClientes getGestorClientes() {
         return gestorClientes;
+    }
+    public GestorEspacios getGestorEspacios() {
+        return gestorEspacios;
+    }
+    public GestorServicios getGestorServicios() {
+        return gestorServicios;
+    }
+    public GestorEmpleados getGestorEmpleados() {
+        return gestorEmpleados;
+    }
+    public GestorContratos getGestorContratos() {
+        return gestorContratos;
     }
 }
