6aff2b02a4967c88914abdac25740986363651f7
Kevin <kevinmejias866@gmail.com>
2026-08-26 22:48:24 -0600

    VentanaEmpleado y VentanaBuscarEmpleado
    Se desarrollan las ventanas FrmVentanaEmpleado y FrmVentanaBuscarEmpleado (diseño y funcionalidad)

--- src/storagebox/StorageBox.java
+++ src/storagebox/StorageBox.java
@@ -4,6 +4,8 @@
  */
 package storagebox;
 
+
+
 /**
  *
  * @author kevin
@@ -14,7 +16,7 @@ public class StorageBox {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+          
     }
     
 }
